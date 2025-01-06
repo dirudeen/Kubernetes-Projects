c9cfe50cfcaae5e4ba5eec96ba0bf55359cca928
dirudeen <dirudeen22@gmail.com>
2025-01-06 20:19:31 +0300

    feat: Add liveness and readiness endpoints to the application

--- bun-app/src/index.ts
+++ bun-app/src/index.ts
@@ -1,14 +1,24 @@
-import { Hono } from 'hono'
-import { serveStatic } from "hono/bun"
-const app = new Hono()
+import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+const app = new Hono();
 
-app.use('/', serveStatic({ path: './static/index.html' }))
+app.use("/", serveStatic({ path: "./static/index.html" }));
 
-app.get('*', (c) => {
-  return c.redirect("/")
-})
+// check if the app is healthy
+app.get("/health", (c) => {
+  return c.text("OK");
+});
 
-export default { 
-  port: 3000, 
-  fetch: app.fetch, 
-} 
+// check if the app is ready to serve
+app.get("/ready", (c) => {
+  return c.text("OK");
+});
+
+app.get("*", (c) => {
+  return c.redirect("/");
+});
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+};
